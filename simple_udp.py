6490fd5d649e5fe2767ba62c2ff3327535551d51
Benoît Ganne <bganne@cisco.com>
2019-08-14 12:41:55 +0200

    add ipv6 in simple_udp

--- simple_udp.py
+++ simple_udp.py
@@ -7,3 +7,7 @@ with PcapWriter(sys.stdout) as pcap:
              IP(src='192.168.1.%i' % i, dst='192.168.2.%i' % i) /
              UDP(sport=1000+i, dport=2000+i))
         pcap.write(p / Raw('\x0a' * (64 - len(p))))
+        p = (Ether(src='00:11:11:11:11:%x' % i, dst='00:22:22:22:22:%x' % i) /
+             IPv6(src='192:168:1::%i' % i, dst='192:168:2::%i' % i) /
+             UDP(sport=1000+i, dport=2000+i))
+        pcap.write(p / Raw('\x0a' * (64 - len(p))))
